64d00854eb64832e51e596c762b70a5eb6491e79
Chris <chris@deep-blue.io>
2022-10-07 16:12:33 +0200

    docs: add info on args and logs

--- conductor.js
+++ conductor.js
@@ -1,7 +1,7 @@
 const { execSync } = require('node:child_process');
 
 // comand for execution
-// node conductor [urlTarget] [env]
+// node conductor [urlTarget] [env?]
 
 (() => {
   const urlTarget = process.argv[2];
@@ -15,7 +15,7 @@ const { execSync } = require('node:child_process');
     console.log('Conductor has stoped script, you probably dont want him to try to turn of your computer ;)');
     return;
   } else if (envComand === 'dev') {
-    console.log('Finished running, will not stop instanse but will no longer to anything. (its not cause im lazy ,:| ')
+    console.log('Finished running, will not stop instanse but will no longer to anything.')
   }
 
   // This stops the instance (turns it off)!
