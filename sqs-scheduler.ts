055d3b0fcd8d7425a8a31483c7ed203e24bc870d
Chris <chris@deep-blue.io>
2022-10-09 22:10:59 +0200

    feat: add queue puller

---
+++ sqs-scheduler.ts
@@ -0,0 +1,20 @@
+import { SQSClient, ReceiveMessageCommand, ReceiveMessageCommandOutput } from '@aws-sdk/client-sqs';
+
+export async function takeNextScheduledAudit(): Promise<string | undefined> {
+  
+  const client = new SQSClient({ region: "us-east-1" });
+  const params = {
+    QueueUrl: "https://sqs.us-east-1.amazonaws.com/495685399379/scheduled-userflows"
+  };
+
+  const command = new ReceiveMessageCommand(params);
+  const response = await new Promise<ReceiveMessageCommandOutput>(async (resolve) => {
+      resolve(await client.send(command))
+  });
+  console.log(response);
+  if (response.Messages) {
+    return response.Messages[0]?.Body
+  }
+}
+
+takeNextScheduledAudit();
