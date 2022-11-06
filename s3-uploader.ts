a2e95491cfc747c64f02fb279c61f18e66d6e4f2
Chris <chris@deep-blue.io>
2022-11-06 19:54:34 +0100

    fix: get results web url

--- s3-uploader.ts
+++ s3-uploader.ts
@@ -1,25 +1,48 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import { readFileSync, unlinkSync } from 'fs';
-import { S3Client, PutObjectCommand, PutObjectCommandOutput } from '@aws-sdk/client-s3';
+import {
+  S3Client,
+  PutObjectCommand,
+  PutObjectCommandOutput,
+  GetObjectCommand,
+  GetObjectCommandInput
+} from '@aws-sdk/client-s3';
 
-export async function uploadResultsToBucket(urlString: string): Promise<PutObjectCommandOutput> {
+export async function uploadResultsToBucket(urlString: string): Promise<string> {
   const filePath = './measures/deep-blue-performance-test.uf.html';
   const recordBody = readFileSync(filePath);
   const recordKey = getRecordKey(urlString);
   const uploadResponse = await uploadRecord(recordKey, recordBody);
   unlinkSync(filePath);
+  console.log(uploadResponse);
   return uploadResponse;
 }
 
 function getRecordKey(urlString: string): string {
   const url = new URL(urlString);
   const timestamp = new Date().toISOString().slice(0,16).replace(":", "_");
-  return `${timestamp}${url.hostname}.uf.html`
+  const hash = generateSimpleHash(20);
+  return `${timestamp}${url.hostname}${hash}.uf.html`;
 }
 
-async function uploadRecord(recordKey: string, recordBody: Buffer): Promise<PutObjectCommandOutput> {
+function generateSimpleHash(length: number): string {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for ( let i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+async function uploadRecord(recordKey: string, recordBody: Buffer): Promise<string> {
   const client = new S3Client({region: 'eu-central-1'});
+  await putRecordInBucket(client, recordKey, recordBody);
+  return await getRecordUrl(client, recordKey);
+}
+
+async function putRecordInBucket(client: S3Client, recordKey: string, recordBody: Buffer): Promise<PutObjectCommandOutput> {
   const params = {
     Bucket: 'deepblue-userflow-records',
     Key: recordKey,
@@ -29,4 +52,15 @@ async function uploadRecord(recordKey: string, recordBody: Buffer): Promise<PutO
   };
   const command = new PutObjectCommand(params);
   return await client.send(command);
+}
+
+async function getRecordUrl(client: S3Client, recordKey: string): Promise<string> {
+  const params: GetObjectCommandInput = {
+    Bucket: 'deepblue-userflow-records',
+    Key: recordKey
+  }
+  const command = new GetObjectCommand(params);
+  const response = await client.send(command);
+  console.log(response);
+  return response.WebsiteRedirectLocation as string;
 }
