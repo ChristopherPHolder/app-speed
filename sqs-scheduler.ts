d3e5e780bc13891cde5037e97a55008c724f209a
Chris <chris@deep-blue.io>
2022-11-06 21:45:25 +0100

    fix: add wait time for sqs poll

--- sqs-scheduler.ts
+++ sqs-scheduler.ts
@@ -1,11 +1,11 @@
-import { SQSClient, ReceiveMessageCommand } from '@aws-sdk/client-sqs';
+import {SQSClient, ReceiveMessageCommand, ReceiveMessageCommandInput} from '@aws-sdk/client-sqs';
 import {AuditRunParams} from './types';
 
 export async function takeNextScheduledAudit(): Promise<AuditRunParams|void> {
-  
   const client = new SQSClient({ region: "us-east-1" });
-  const params = {
-    QueueUrl: "https://sqs.us-east-1.amazonaws.com/495685399379/scheduled-userflows"
+  const params: ReceiveMessageCommandInput = {
+    QueueUrl: "https://sqs.us-east-1.amazonaws.com/495685399379/scheduled-userflows",
+    WaitTimeSeconds: 60,
   };
 
   const command = new ReceiveMessageCommand(params);
