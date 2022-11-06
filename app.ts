29906e0ed539e88f8ab550bd8bccdbe3807d287e
Chris <chris@deep-blue.io>
2022-11-06 21:46:12 +0100

    refactor: add import spaces for linter

--- app.ts
+++ app.ts
@@ -1,8 +1,8 @@
 import { execSync } from 'node:child_process';
 import { takeNextScheduledAudit } from './sqs-scheduler';
 import { uploadResultsToBucket } from './s3-uploader';
-import {AuditRunParams} from "./types";
-import {sendAuditResults} from "./sendResults";
+import { AuditRunParams } from "./types";
+import { sendAuditResults } from "./sendResults";
 
 (async function conductor(): Promise<void> {
     const nextAuditRunParams = await takeNextScheduledAudit();
