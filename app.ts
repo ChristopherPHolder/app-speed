e1048e16cafc663f39ee7db74fea0d1f74eb2992
Chris <chris@deep-blue.io>
2022-11-08 15:36:13 +0100

    fix: specify execution shell in child process

--- app.ts
+++ app.ts
@@ -7,12 +7,12 @@ import { sendAuditResults } from "./sendResults";
 (async function conductor(): Promise<void> {
     const nextAuditRunParams = await takeNextScheduledAudit();
     if (!nextAuditRunParams) {
-        execSync("sudo shutdown -h && exit 0");
+        execSync('sudo shutdown -h && exit 0', {shell: '/bin/bash'});
     }
 
     try {
         const { targetUrl, requesterId, endpoint } = nextAuditRunParams as AuditRunParams;
-        execSync(`npx user-flow --url=${targetUrl} --open=false`);
+        execSync(`npx user-flow --url=${targetUrl} --open=false`, {shell: '/bin/bash'});
         const resultsUrl = await uploadResultsToBucket(targetUrl);
         await sendAuditResults(requesterId, endpoint, resultsUrl);
     } catch (error) {
