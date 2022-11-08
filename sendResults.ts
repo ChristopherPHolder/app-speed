e67ebeea3b0a2853ad78562301ae5bf23a5c01c7
Chris <chris@deep-blue.io>
2022-11-08 19:39:16 +0100

    fix: improve responses data

--- sendResults.ts
+++ sendResults.ts
@@ -1,26 +1,32 @@
+import type {
+	ApiGatewayManagementApiClientConfig,
+	PostToConnectionCommandInput,
+	PostToConnectionCommandOutput,
+} from '@aws-sdk/client-apigatewaymanagementapi';
 import {
-    ApiGatewayManagementApiClient,
-    ApiGatewayManagementApiClientConfig,
-    PostToConnectionCommand, PostToConnectionCommandInput, PostToConnectionCommandOutput
-} from "@aws-sdk/client-apigatewaymanagementapi";
+	ApiGatewayManagementApiClient,
+	PostToConnectionCommand,
+} from '@aws-sdk/client-apigatewaymanagementapi';
+import type {RunnerResponseMessage} from './types';
 
 export async function sendAuditResults(
-    connectionId: string,
-    endpoint: string,
-    resultsUrl: string
+	connectionId: string,
+	endpoint: string,
+	resultsUrl: string,
 ): Promise<PostToConnectionCommandOutput> {
-    const config:  ApiGatewayManagementApiClientConfig = {
-        region: 'us-east-1',
-        endpoint: `https://${endpoint}`,
-    }
-    const client = new ApiGatewayManagementApiClient(config);
-    const input: PostToConnectionCommandInput = {
-        //@ts-ignore // Wrong typing in sdk
-        Data: resultsUrl,
-        ConnectionId: connectionId,
-    }
-    const command = new PostToConnectionCommand(input);
-    const response = await client.send(command);
-    console.log(response);
-    return response;
+
+	const responseData: RunnerResponseMessage = {
+		action: 'completed',
+		message: 'Successfully completed the User Flow Audit',
+		reports: {htmlReportUrl: resultsUrl},
+	};
+
+	const config: ApiGatewayManagementApiClientConfig = {region: 'us-east-1', endpoint: `https://${endpoint}`};
+	const client = new ApiGatewayManagementApiClient(config);
+
+	// @ts-expect-error // Wrong typing in aws sdk
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	const input: PostToConnectionCommandInput = {Data: responseData, ConnectionId: connectionId};
+	const command = new PostToConnectionCommand(input);
+	return client.send(command);
 }
