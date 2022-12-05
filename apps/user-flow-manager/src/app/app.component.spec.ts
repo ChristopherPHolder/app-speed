b9c8806e839d6589bfac61adc821a29c54171bf7
Chris <chris@deep-blue.io>
2022-12-05 07:18:53 +0100

    fix: remove nx template component

--- apps/user-flow-manager/src/app/app.component.spec.ts
+++ apps/user-flow-manager/src/app/app.component.spec.ts
@@ -1,13 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent, NxWelcomeComponent],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -23,10 +22,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('user-flow-manager');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome user-flow-manager');
-  });
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector('h1')?.textContent).toContain('Welcome user-flow-manager');
+  // });
 });
