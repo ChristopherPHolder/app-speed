d6b9452448b7c21d5aacad7fbd80f70ea4e5c282
Chris <chris@deep-blue.io>
2023-01-08 16:27:13 -0600

    feat: add simple audit adapter and container

--- libs/features/simple-audit/src/lib/simple-audit/simple-audit.container.ts
+++ libs/features/simple-audit/src/lib/simple-audit/simple-audit.container.ts
@@ -1,14 +1,42 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UserFlowFormComponent } from 'core-ui';
+import { ResultsDisplayComponentModule, UserFlowFormComponent } from 'core-ui';
+import { RxState } from '@rx-angular/state';
+import { SimpleAuditAdapter } from './simple-audit.adapter';
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+import { ResultProgress } from 'shared';
+import { map } from 'rxjs';
+
+type ContainerState = {
+  progress: ResultProgress;
+  htmlReportUrl?: string;
+}
 
 @Component({
   selector: 'app-simple-audit',
   standalone: true,
-  imports: [CommonModule, UserFlowFormComponent],
+  imports: [CommonModule, UserFlowFormComponent, ResultsDisplayComponentModule],
   templateUrl: './simple-audit.container.html',
   styleUrls: ['./simple-audit.container.scss'],
   encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [RxState]
 })
-export class SimpleAuditContainer {}
+export class SimpleAuditContainer {
+  constructor(
+    private adapter: SimpleAuditAdapter,
+    public state: RxState<ContainerState>
+  ) {
+    this.state.connect(
+      'htmlReportUrl',
+      this.adapter.results$.pipe(map(({ htmlReportUrl }) => htmlReportUrl))
+    )
+    this.state.connect(
+      'progress',
+      this.adapter.progress$
+    )
+  }
+  runAudit(auditUrl: string) {
+    this.adapter.handleAudit(auditUrl);
+  }
+}
