4704e32102cdac9c96102a2a08b11bddf378ecd3
Michael Hladky <michael@hladky.at>
2023-01-11 05:10:40 +0100

    feat: add NetworkConnection service

--- libs/shared/src/lib/environments/environment.ts
+++ libs/shared/src/lib/environments/environment.ts
@@ -7,5 +7,6 @@ export const environment = {
     url: 'https://deepblue-userflow-records.s3.eu-central-1.amazonaws.com/',
     name: 'deepblue-userflow-records',
   },
-  ufoSocketUrl: 'wss://5ag9xf0aab.execute-api.us-east-1.amazonaws.com/ufo'
+  ufoSocketUrl: 'wss://5ag9xf0aab.execute-api.us-east-1.amazonaws.com/ufo',
+  isOnlineApi: 'https://deepblue-userflow-records.s3.eu-central-1.amazonaws.com/'
 };
