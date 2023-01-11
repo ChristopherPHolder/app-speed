fb3950251467262e39c62525b1d0f1523af3e01b
Michael Hladky <michael@hladky.at>
2023-01-11 07:33:05 +0100

    refactor: change isOfflien API URL

--- libs/shared/src/lib/environments/environment.ts
+++ libs/shared/src/lib/environments/environment.ts
@@ -8,5 +8,5 @@ export const environment = {
     name: 'deepblue-userflow-records',
   },
   ufoSocketUrl: 'wss://5ag9xf0aab.execute-api.us-east-1.amazonaws.com/ufo',
-  isOnlineApi: 'https://deepblue-userflow-records.s3.eu-central-1.amazonaws.com/'
+  isOnlineApi: 'https://ib3ncok6l1.execute-api.us-east-1.amazonaws.com/Prod/is-online'
 };
