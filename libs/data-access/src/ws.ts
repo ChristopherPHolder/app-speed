7f542f92e72232a86161c1b23cedfa881aa22e9b
Michael Hladky <michael@hladky.at>
2023-01-11 10:57:22 +0100

    refactor: type I/O websocked messages

--- libs/data-access/src/ws.ts
+++ libs/data-access/src/ws.ts
@@ -1,23 +1,22 @@
 import { Injectable } from '@angular/core';
-import { distinctUntilChanged, Subject, switchAll } from 'rxjs';
-import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+import { Observable, Subject, switchAll } from 'rxjs';
+import { webSocket, WebSocketSubject, WebSocketSubjectConfig } from 'rxjs/webSocket';
 import { RxEffects } from '@rx-angular/state/effects';
 import { NETWORK_INFORMATION_TYPE, NetworkConnection } from './network-connetion';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Ws<T> extends RxEffects {
-  private readonly _ws = new Subject<WebSocketSubject<T>>();
-  private url = '';
-  private webSocket: WebSocketSubject<T> | undefined = undefined;
-  readonly messages$ = this._ws.pipe(switchAll());
+export class Ws<I, O = I> extends RxEffects {
+  private readonly _ws = new Subject<WebSocketSubject<I | O>>();
+  private webSocket: WebSocketSubject<I | O> | undefined = undefined;
+  readonly messages$: Observable<I> = this._ws.pipe(switchAll()) as Observable<I>;
 
   constructor(private readonly networkConnection: NetworkConnection) {
     super();
   }
 
-  init(url: string) {
+  init(url: string | WebSocketSubjectConfig<I | O>) {
     if (!url) {
       throw new Error('WS needs a URL to connect to.');
     }
@@ -26,15 +25,28 @@ export class Ws<T> extends RxEffects {
     }
 
     this.register(
-      this.networkConnection.connectionType$.pipe(distinctUntilChanged()),
+      this.networkConnection.connectionType$,
       connection => {
         if (connection === NETWORK_INFORMATION_TYPE.WIFI) {
-          this.webSocket = webSocket<T>(url);
+          this.webSocket = webSocket<I | O>(url);
           this._ws.next(this.webSocket);
         } else {
           this.webSocket && this.webSocket.complete();
         }
       }
     );
+  }
+
+  close() {
+    if (!this.webSocket) {
+      throw new Error('WS not initialized');
+    }
+    this.webSocket.complete();
+  }
+  send(msg: O) {
+    if (!this.webSocket) {
+      throw new Error('WS not initialized');
+    }
+    this.webSocket.next(msg);
   }
 }
--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -1,7 +1,5 @@
 export * from './lib/types';
-
 export * from './lib/environments/environment';
-
 export * from './lib/bypass-src.directive';
-
 export * from './lib/constants';
+export * from './lib/ws-types';
