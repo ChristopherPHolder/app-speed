52f97181abc517052d349dae70f3669107f0a2ea
Chris <chris@deep-blue.io>
2023-02-03 01:53:58 +0100

    refactor: organize files

---
+++ libs/shared/src/lib/directives/bypass-src.directive.spec.ts
@@ -0,0 +1,11 @@
+import { BypassSrcDirective } from './bypass-src.directive';
+
+// @ TODO - add basic test
+describe('BypassSrcDirective', () => {
+  it('should create an instance', () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const directive = new BypassSrcDirective();
+    expect(directive).toBeTruthy();
+  });
+});
