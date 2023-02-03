b36e6d207d331c3b9eb8f963b257345cce2e6f51
Chris <chris@deep-blue.io>
2023-02-03 16:18:29 +0100

    refactor: remove unnecessary import

--- libs/ui/src/component/loading-spinner/loading-spinner.component.ts
+++ libs/ui/src/component/loading-spinner/loading-spinner.component.ts
@@ -1,10 +1,8 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-loading-spinner',
   standalone: true,
-  imports: [CommonModule],
   template: `
     <div class="lds-roller">
       <div></div>
