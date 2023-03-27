f69a45748fb6d3e4d3a8bf6fddce031a2dd843e9
Chris <chris@deep-blue.io>
2023-03-27 18:38:40 +0200

    feat: add radial chart to display scores

---
+++ libs/ui/src/component/radial-chart/radial-chart.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-radial-chart',
+  standalone: true,
+  template: `
+      <div class="radial-chart {{getColorScheme()}}">
+          <svg viewBox="0 0 100 100">
+              <circle cx="50" cy="50" r="45" class="background" />
+              <circle cx="50" cy="50" r="45" class="progress" [attr.stroke-dashoffset]="getOffset()"/>
+          </svg>
+          <span class="score-text">{{ score }}</span>
+      </div>
+  `,
+  styleUrls: ['./radial-chart.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
+})
+export class RadialChartComponent {
+  @Input() score = 0;
+
+  getColorScheme(): 'green' | 'orange' | 'red' {
+    if (this.score > 90) return 'green';
+    if (this.score > 50) return 'orange';
+    return 'red';
+  }
+  getOffset(): number {
+    const progress = (100 - 50) / 100 ;
+    const circumference = 2 * Math.PI * 45;
+    return progress * circumference;
+  }
+}
--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,3 +1,4 @@
 export * from './component/results-display/results-display.component';
+export * from './component/radial-chart/radial-chart.component';
 export * from './component/user-flow-form/user-flow-form.component';
 export * from './component/header/header.component';
