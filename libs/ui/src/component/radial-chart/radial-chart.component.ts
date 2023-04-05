69b1938f622afe3bbb20c31f7cffdefde46ff0ee
Chris <chris@deep-blue.io>
2023-04-05 23:44:04 +0200

    fix: browser compatibility for svgs

--- libs/ui/src/component/radial-chart/radial-chart.component.ts
+++ libs/ui/src/component/radial-chart/radial-chart.component.ts
@@ -5,7 +5,7 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   standalone: true,
   template: `
       <div class="radial-chart {{getColorScheme()}}">
-          <svg viewBox="0 0 100 100">
+          <svg viewBox="0 0 100 100" >
               <circle cx="50" cy="50" r="45" class="background" />
               <circle cx="50" cy="50" r="45" class="progress" [attr.stroke-dashoffset]="getOffset()"/>
           </svg>
