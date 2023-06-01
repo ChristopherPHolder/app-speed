234836d3c011a44e243850ee2d61c9b93f057751
Chris <chris@deep-blue.io>
2023-06-01 20:28:44 +0200

    chore: nx migrate 16.2.2

--- apps/runner/src/main.ts
+++ apps/runner/src/main.ts
@@ -1,19 +1,19 @@
-import {takeNextScheduledAudit} from './app/queue';
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import {execSync} from 'node:child_process';
+import { takeNextScheduledAudit } from './app/queue';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { execSync } from 'node:child_process';
 import { uploadResultsToBucket } from './app/store';
 import { sendAuditResults } from './app/results';
-import {UserFlowRunner} from 'user-flow-runner';
+import { UserFlowRunner } from 'user-flow-runner';
 
 (async function run(): Promise<void> {
   const nextAuditRunParams = await takeNextScheduledAudit();
   if (!nextAuditRunParams) {
-    execSync('shutdown -h -t 5 & exit 0', {shell: '/bin/bash'});
+    execSync('shutdown -h -t 5 & exit 0', { shell: '/bin/bash' });
     return;
   }
 
   try {
-    const {targetUrl, requesterId, endpoint} = nextAuditRunParams;
+    const { targetUrl, requesterId, endpoint } = nextAuditRunParams;
     const auditResults = await new UserFlowRunner().run({ targetUrl });
     const resultsUrl = await uploadResultsToBucket(targetUrl, auditResults);
     await sendAuditResults(requesterId, endpoint, resultsUrl);
