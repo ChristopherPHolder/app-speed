e0165cbd9f4ac0ccda81bb647c8331507e18ce7f
ChristopherPHolder <toholderandrews@gmail.com>
2023-07-23 10:56:51 +0200

    feat: add user flow replay auditor

--- libs/user-flow-replay/src/lib/user-flow-replay.spec.ts
+++ libs/user-flow-replay/src/lib/user-flow-replay.spec.ts
@@ -1,8 +1,28 @@
 import { describe, it, expect } from 'vitest';
-import { userFlowReplay } from './user-flow-replay';
+import { UserFlowAudit, UserFlowAuditConfiguration } from './user-flow-replay';
 
 describe('userFlowReplay', () => {
-  it('should work', () => {
-    expect(userFlowReplay()).toEqual('user-flow-replay');
-  });
+  it('should work', async () => {
+
+    const replayScript = {
+      title: 'Example Title',
+      steps: [
+        {
+          type: 'navigate',
+          url: 'https://google.com',
+          name: 'Navigate to Google.com'
+        }
+      ]
+    }
+
+    const auditConfiguration: UserFlowAuditConfiguration = {
+      options: {},
+      replayScript: replayScript
+    };
+
+    const userFlowAudit = new UserFlowAudit(auditConfiguration);
+    const results = await userFlowAudit.run();
+
+    expect(results.jsonReport.name).toEqual(replayScript.title);
+  }, 50_000);
 });
--- libs/user-flow-replay/src/lib/user-flow-replay.ts
+++ libs/user-flow-replay/src/lib/user-flow-replay.ts
@@ -1,4 +1,65 @@
+import { createRunner, Runner, UserFlow as PRUserFlow } from '@puppeteer/replay';
+import { UserFlowRunnerExtension } from './runner-extension';
+import { parse } from './parse';
+import { Browser, launch, Page } from 'puppeteer';
+import { FlowResult as LHFlowResult, startFlow, UserFlow as LHUserFlow } from 'lighthouse';
 
-export function userFlowReplay(): string {
-  return 'user-flow-replay';
+export type UserFlowRunnerContext = {
+  browser: Browser;
+  page: Page;
+  flow: LHUserFlow;
+};
+
+export type UserFlowAuditResult = {
+  jsonReport: LHFlowResult;
+  htmlReport: string;
+}
+
+export type UserFlowAuditConfiguration = {
+  options: object;
+  replayScript: object;
+}
+
+export class UserFlowAudit {
+  private readonly replayScript;
+
+  constructor(configuration: UserFlowAuditConfiguration) {
+    this.replayScript = parse(configuration.replayScript);
+  }
+
+  public async run() {
+    const runnerContext = await this.initializeRunnerContext();
+    const replayRunner = await this.createRunner(runnerContext);
+    await this.executeRunner(replayRunner);
+    const results = await this.extractResults(runnerContext);
+    await this.terminateRunnerContext(runnerContext);
+    return results;
+  }
+
+  private async initializeRunnerContext(): Promise<UserFlowRunnerContext> {
+    const browser = await launch({ headless: false });
+    const page = await browser.newPage();
+    const flow = await startFlow(page, { name: this.replayScript.title });
+    return {browser, page, flow};
+  }
+
+  private async createRunner(runnerContext: UserFlowRunnerContext): Promise<Runner> {
+    const { browser, page, flow } = runnerContext;
+    const runnerExtension = new UserFlowRunnerExtension(browser, page, flow);
+    return await createRunner(this.replayScript as PRUserFlow, runnerExtension);
+  }
+
+  private async executeRunner(replayRunner: Runner): Promise<void> {
+    await replayRunner.run();
+  }
+
+  private async extractResults(runnerContext: UserFlowRunnerContext): Promise<UserFlowAuditResult> {
+    const jsonReport = await runnerContext.flow.createFlowResult();
+    const htmlReport = await runnerContext.flow.generateReport();
+    return {jsonReport, htmlReport};
+  }
+
+  private async terminateRunnerContext(runnerContext: UserFlowRunnerContext): Promise<void> {
+    await runnerContext.browser.close();
+  }
 }
