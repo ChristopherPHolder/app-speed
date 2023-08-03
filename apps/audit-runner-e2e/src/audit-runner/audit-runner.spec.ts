0d0e05b0752c04a9ca3ae00002be0bb747e2e986
ChristopherPHolder <toholderandrews@gmail.com>
2023-08-03 23:50:59 +0200

    feat: add esm audit runner

---
+++ apps/audit-runner-e2e/src/audit-runner/audit-runner.spec.ts
@@ -0,0 +1,13 @@
+import { describe, it, expect } from 'vitest';
+import { execSync } from 'child_process';
+import { join } from 'path';
+
+describe('CLI tests', () => {
+  it('should print a message', () => {
+    const cliPath = join(process.cwd(), 'dist/apps/audit-runner');
+
+    const output = execSync(`node ${cliPath}`).toString();
+
+    expect(output).toMatch(/Hello World/);
+  });
+});
