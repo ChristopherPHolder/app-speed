6e195fd7e304d94dc3e05967619bcc59df78dbb9
ChristopherPHolder <toholderandrews@gmail.com>
2023-08-18 20:53:36 +0200

    feat: fix test config

--- apps/audit-runner/vite.config.ts
+++ apps/audit-runner/vite.config.ts
@@ -22,11 +22,12 @@ export default defineConfig({
   // },
 
   test: {
+    threads: false,
     globals: true,
     cache: {
       dir: '../../node_modules/.vitest',
     },
-    environment: 'jsdom',
+    environment: 'node',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
 });
