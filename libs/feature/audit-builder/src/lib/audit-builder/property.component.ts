b737f276a52d47a5d9a20bc1bbff12565595ad64
ChristopherPHolder <toholderandrews@gmail.com>
2024-01-17 19:56:16 +0100

    fix(build-audit): remove unused and lint

--- libs/feature/audit-builder/src/lib/audit-builder/property.component.ts
+++ libs/feature/audit-builder/src/lib/audit-builder/property.component.ts
@@ -1,17 +1,7 @@
 import { ChangeDetectionStrategy, Component, computed, inject, Input, signal } from '@angular/core';
 
-
-import { MatInputModule } from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatOptionModule } from '@angular/material/core';
-import { MatSelectModule } from '@angular/material/select';
-import { RxFor } from '@rx-angular/template/for';
-import { PropertyName, StepType } from '../schema/audit-builder.types';
 import { AuditBuilderService } from './audit-builder.service';
-import { INPUT_TYPE, PROPERTY_NAME } from '../schema/step-property.constants';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { JsonPipe } from '@angular/common';
+
 import { InputStringArrayComponent } from '../property-input/input-string-array.component';
 import { InputStringComponent } from '../property-input/input-string.component';
 import { InputNumberComponent } from '../property-input/input-number.component';
@@ -19,73 +9,68 @@ import { InputBooleanComponent } from '../property-input/input-boolean.component
 import { InputOptionsComponent } from '../property-input/input-options.component';
 import { InputRecordsComponent } from '../property-input/input-records.component';
 
+import { INPUT_TYPE, PROPERTY_NAME } from '../schema/step-property.constants';
+
+import { PropertyName, StepType } from '../schema/audit-builder.types';
+
 @Component({
   selector: 'lib-step-property',
   standalone: true,
   imports: [
-    MatInputModule,
-    ReactiveFormsModule,
-    MatOptionModule,
-    MatSelectModule,
-    RxFor,
-    MatIconModule,
-    MatButtonModule,
-    JsonPipe,
-    InputStringArrayComponent,
     InputStringComponent,
     InputNumberComponent,
     InputBooleanComponent,
     InputOptionsComponent,
+    InputStringArrayComponent,
     InputRecordsComponent,
   ],
   template: `
-      @switch (schema().inputType) {
-          @case (INPUT_TYPE.STRING) {
-            <lib-input-string 
-              [schema]='schema()' 
-              [control]='$any(control())'
-              (deleteProperty)='handleDeleteProperty()'
-            />
-          }
-          @case (INPUT_TYPE.NUMBER) {
-            <lib-input-number
-              [schema]='schema()'
-              [control]='$any(control())'
-              (deleteProperty)='handleDeleteProperty()'
-            />
-          }
-          @case (INPUT_TYPE.BOOLEAN) {
-            <lib-input-boolean
-              [schema]='schema()'
-              [control]='$any(control())'
-              (deleteProperty)='handleDeleteProperty()'
-            />
-          }
-          @case (INPUT_TYPE.OPTIONS) {
-            <lib-input-options
-              [schema]='schema()'
-              [control]='$any(control())'
-              (deleteProperty)='handleDeleteProperty()'
-              (selectedChange)='handleSelectedChange($event)'
-            />
-          }
-          @case (INPUT_TYPE.STRING_ARRAY) {
-            <lib-input-string-array 
-              [schema]='schema()' 
-              [control]='$any(control())'
-              (deleteProperty)='handleDeleteProperty()'
-            />
-          }
-          @case (INPUT_TYPE.RECORDS) {
-            <lib-input-records
-              [schema]='schema()'
-              [control]='$any(control())'
-              (deleteProperty)='handleDeleteProperty()'
-            />
-          }
+    @switch (schema().inputType) {
+      @case (INPUT_TYPE.STRING) {
+        <lib-input-string 
+          [schema]='schema()' 
+          [control]='$any(control())'
+          (deleteProperty)='handleDeleteProperty()'
+        />
       }
+      @case (INPUT_TYPE.NUMBER) {
+        <lib-input-number
+          [schema]='schema()'
+          [control]='$any(control())'
+          (deleteProperty)='handleDeleteProperty()'
+        />
+      }
+      @case (INPUT_TYPE.BOOLEAN) {
+        <lib-input-boolean
+          [schema]='schema()'
+          [control]='$any(control())'
+          (deleteProperty)='handleDeleteProperty()'
+        />
+      }
+      @case (INPUT_TYPE.OPTIONS) {
+        <lib-input-options
+          [schema]='schema()'
+          [control]='$any(control())'
+          (deleteProperty)='handleDeleteProperty()'
+          (selectedChange)='handleSelectedChange($event)'
+        />
+      }
+      @case (INPUT_TYPE.STRING_ARRAY) {
+        <lib-input-string-array 
+          [schema]='schema()' 
+          [control]='$any(control())'
+          (deleteProperty)='handleDeleteProperty()'
+        />
+      }
+      @case (INPUT_TYPE.RECORDS) {
+        <lib-input-records
+          [schema]='schema()'
+          [control]='$any(control())'
+          (deleteProperty)='handleDeleteProperty()'
+        />
+      }
+    }
   `,
-  styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StepPropertyComponent {
