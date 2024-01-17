f4671f786f373e3bc005f198cf53e0b22ecda63d
ChristopherPHolder <toholderandrews@gmail.com>
2024-01-17 20:07:50 +0100

    fix(build-audit): add missing ui pieces

--- libs/feature/audit-builder/src/lib/property-input/input-string-array.component.ts
+++ libs/feature/audit-builder/src/lib/property-input/input-string-array.component.ts
@@ -13,19 +13,30 @@ import { StepProperty } from '../schema/audit-builder.types';
   imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
   template: `
     <!-- TODO Improve Styling For Input Section -->
+    <!-- One potential solution is to simple add a -> icon next to the items -->
     <div>
       <!-- TODO Improve Styling For Input Section Title -->
-      <h4>{{ schema.name }}</h4>
-      @if (!schema.required) {
-        <button mat-icon-button aria-label="Delete property from step" (click)='deleteProperty.emit()'>
-          <mat-icon>delete</mat-icon>
-        </button>
-      }
+      <h4>
+        {{ schema.name }}
+        @if (!schema.required) {
+          <!-- TODO Add the functionality to add inputs -->
+          <button mat-icon-button aria-label="Add property to step">
+            <mat-icon>library_add</mat-icon>
+          </button>
+          <button mat-icon-button aria-label="Delete property from step" (click)='deleteProperty.emit()'>
+            <mat-icon>delete</mat-icon>
+          </button>
+        }
+      </h4>
       @for (control of control.controls; track control) {
+        <mat-icon>subdirectory_arrow_right</mat-icon>
         <mat-form-field>
           <input matInput [formControl]='control'>
-          <!-- TODO Add the ability to add and delete inputs -->
+          <!-- TODO Add the functionality to delete inputs -->
         </mat-form-field>
+        <button mat-icon-button aria-label="Delete property from step">
+          <mat-icon>delete</mat-icon>
+        </button>
       }
     </div>
   `,
