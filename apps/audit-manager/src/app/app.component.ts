e3ce9819e89ca11536dab7498addf3cde0c2c311
ChristopherPHolder <toholderandrews@gmail.com>
2024-03-03 19:13:26 +0100

    feat: fix nav issue

--- apps/audit-manager/src/app/app.component.ts
+++ apps/audit-manager/src/app/app.component.ts
@@ -1,22 +1,31 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { ChangeDetectionStrategy, Component, inject, ViewEncapsulation } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { ShellComponent } from 'ui/shell';
 
 @Component({
   standalone: true,
   imports: [ShellComponent, RouterModule],
   selector: 'app-root',
-  template: `<ui-shell [navItems]='navItems'><router-outlet/></ui-shell>`,
+  template: `
+    <ui-shell 
+      [navItems]='navItems'
+      (navClick)='navigateTo($event)'
+    >
+      <router-outlet/>
+    </ui-shell>`,
   styles: [''],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
+  router = inject(Router);
   title = 'audit-manager';
 
   public readonly navItems = [
-    'Link 1',
-    'Link 2',
-    'Link 3'
+    'results-viewer',
+    'audit-builder',
   ]
+  navigateTo(location: string): void {
+    this.router.navigate([location]);
+  }
 }
