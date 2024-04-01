7a9af75e7cb07597358d4993b295eb89da4ac1cb
ChristopherPHolder <toholderandrews@gmail.com>
2024-04-01 08:48:56 +0200

    fix: fix filmstrip  appearance

--- libs/feature/audit-viewer/src/viewer-container/viewer-file-strip.component.ts
+++ libs/feature/audit-viewer/src/viewer-container/viewer-file-strip.component.ts
@@ -4,20 +4,39 @@ import { Component, input } from '@angular/core';
   selector: 'lib-viewer-file-strip',
   template: `
     @for (item of filmStrip(); track item) {
-      <img [src]='item.data' height='100px' alt='' style='padding: 0 10px;'>
+      <img class='film-strip-frame' [src]='item.data' height='100px' alt=''>
     }
   `,
   standalone: true,
   styles: [`
-    :host {
-        display: flex;
-        overflow: auto;
-        -ms-overflow-style: none;  /* Internet Explorer 10+ */
-        scrollbar-width: none;  /* Firefox */
-        ::-webkit-scrollbar {
-            display: none;  /* Safari and Chrome */
-        }
-    }
+      :host {
+          display: flex;
+          overflow: auto;
+          -ms-overflow-style: none; /* Internet Explorer 10+ */
+          scrollbar-width: none; /* Firefox */
+
+          ::-webkit-scrollbar {
+              display: none; /* Safari and Chrome */
+          }
+          padding: 4px;
+      }
+
+      .film-strip-frame {
+          border: groove gray;
+          box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+          border-radius: 8px;
+          transition: transform 0.2s ease, box-shadow 0.2s ease;
+
+          &:not(:first-child):not(:last-child) {
+              margin: 0 8px;
+              /*padding: 16px;*/
+          }
+
+          &:hover {
+              transform: scale(1.05);
+              box-shadow: 0 6px 12px rgba(0, 0, 0, 0.3);
+          }
+      }
   `]
 })
 export class ViewerFileStripComponent {
