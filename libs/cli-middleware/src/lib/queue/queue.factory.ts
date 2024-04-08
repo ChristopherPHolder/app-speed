53a6358c63348091b79d9882baf982ad15af83c4
ChristopherPHolder <toholderandrews@gmail.com>
2024-04-08 08:46:21 +0200

    fix: move audit-queue lib to packages

--- libs/cli-middleware/src/lib/queue/queue.factory.ts
+++ libs/cli-middleware/src/lib/queue/queue.factory.ts
@@ -1,8 +1,8 @@
 import { AuditQueue } from '@ufo/cli-interfaces';
 
 const queueMap = new Map<string, Promise<any>>();
-queueMap.set('local', import('@ufo/audit-queue/local-queue'));
-queueMap.set('sqs', import('@ufo/audit-queue/aws-sqs'));
+queueMap.set('local', import('@app-speed/audit-queue/local-queue'));
+queueMap.set('sqs', import('@app-speed/audit-queue/aws-sqs'));
 
 export async function createAuditQueue(path: string, config?: object): Promise<AuditQueue> {
   const queueImport = queueMap.get(path);
