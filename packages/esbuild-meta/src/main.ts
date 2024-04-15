7713c7a2081862562a9673aadc10977193973dfa
ChristopherPHolder <toholderandrews@gmail.com>
2024-04-15 11:54:14 +0200

    fix(esbuild-meta): make filter default command

--- packages/esbuild-meta/src/main.ts
+++ packages/esbuild-meta/src/main.ts
@@ -5,6 +5,6 @@ import { hideBin } from 'yargs/helpers';
 import { argv } from 'node:process';
 import { filterMetaCommand } from './lib/filter-meta.js';
 
-yargs(hideBin(argv)).command(filterMetaCommand).parse();
+yargs(hideBin(argv)).command('$0', 'Default Command is filter', filterMetaCommand).parse();
 
 console.log('Esbuild Meta Completed Successfully!');
