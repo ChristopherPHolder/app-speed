8a35bfba37ed3dde836c4c58ad24ca3fb708faf6
ChristopherPHolder <toholderandrews@gmail.com>
2024-07-07 09:13:34 +0200

    fix(portal-app): fix style prettier

--- packages/feature/audit-builder/src/audit-builder/audit-step.component.ts
+++ packages/feature/audit-builder/src/audit-builder/audit-step.component.ts
@@ -22,42 +22,42 @@ import { MatFabButton } from '@angular/material/button';
     MatFabButton,
   ],
   template: `
-    <mat-expansion-panel [expanded]='true'>
+    <mat-expansion-panel [expanded]="true">
       <mat-expansion-panel-header>
         <mat-panel-title>
-            @if (builder.steps.at(stepIndex).get('type')!.value; as title) {
-                {{ title | toTitleCase }}
-            } @else {
-                Audit Step Required!
-            }
+          @if (builder.steps.at(stepIndex).get('type')!.value; as title) {
+            {{ title | toTitleCase }}
+          } @else {
+            Audit Step Required!
+          }
         </mat-panel-title>
       </mat-expansion-panel-header>
-      <builder-step-action-dialog [stepIndex]='stepIndex' class='toggle_menu'/>
-        @for (key of builder.getStepPropertyKeys(stepIndex); track key) {
-            <builder-step-property [controlKey]='key' [stepIndex]='stepIndex' />
-        }
-        @for (key of builder.getStepOptionalProperties(stepIndex); track key) {
-            <button mat-fab [extended]='true' color="primary" (click)='builder.addStepProperty(stepIndex, key)'>
-                Add {{ key }}
-            </button>
-        }
+      <builder-step-action-dialog [stepIndex]="stepIndex" class="toggle_menu" />
+      @for (key of builder.getStepPropertyKeys(stepIndex); track key) {
+        <builder-step-property [controlKey]="key" [stepIndex]="stepIndex" />
+      }
+      @for (key of builder.getStepOptionalProperties(stepIndex); track key) {
+        <button mat-fab [extended]="true" color="primary" (click)="builder.addStepProperty(stepIndex, key)">
+          Add {{ key }}
+        </button>
+      }
     </mat-expansion-panel>
   `,
   styles: `
-      .toggle_menu {
-        position: absolute;
-        top: 48px;
-        @media only screen and (min-width: 600px) {
-            right: 16px;
-        }
-        @media only screen and (max-width: 599px) {
-            right: 4px;
-        }
+    .toggle_menu {
+      position: absolute;
+      top: 48px;
+      @media only screen and (min-width: 600px) {
+        right: 16px;
+      }
+      @media only screen and (max-width: 599px) {
+        right: 4px;
+      }
     }
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AuditStepComponent {
-  @Input({required: true}) stepIndex!: number;
+  @Input({ required: true }) stepIndex!: number;
   builder = inject(AuditBuilderService);
 }
