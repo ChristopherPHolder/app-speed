87c07a33e5eab58876a0a653037597f16982d454
ChristopherPHolder <toholderandrews@gmail.com>
2024-08-17 16:46:20 +0200

    fix(workspace): remove fake test

--- packages/userflow-conductor/schedule-audit/src/lib/schedule-audit.spec.ts
+++
@@ -1,7 +0,0 @@
-import { scheduleAudit } from './schedule-audit';
-
-describe('scheduleAudit', () => {
-  it('should work', () => {
-    expect(scheduleAudit()).toEqual('schedule-audit');
-  });
-});
