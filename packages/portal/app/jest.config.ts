b561280a3164ecffd6e1354f563af8d499a3d286
ChristopherPHolder <toholderandrews@gmail.com>
2024-09-14 18:18:14 +0200

    fix(portal): move all to dir

--- packages/portal/app/jest.config.ts
+++ packages/portal/app/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
   displayName: 'audit-manager',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/apps/audit-manager',
+  coverageDirectory: '../../../coverage/apps/audit-manager',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
