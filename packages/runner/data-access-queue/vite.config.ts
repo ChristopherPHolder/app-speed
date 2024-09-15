3284cee6befff9ec3cdbc6039520a5d1804da29b
ChristopherPHolder <toholderandrews@gmail.com>
2024-09-15 08:50:42 +0200

    fix(runner): move runner-queue to dir

--- packages/runner/data-access-queue/vite.config.ts
+++ packages/runner/data-access-queue/vite.config.ts
@@ -4,30 +4,21 @@ import { defineConfig } from 'vite';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  cacheDir: '../../node_modules/.vite/audit-queue',
+  cacheDir: '../../../node_modules/.vite/audit-queue',
 
   plugins: [
     viteTsConfigPaths({
-      root: '../../',
+      root: '../../../',
     }),
   ],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [
-  //    viteTsConfigPaths({
-  //      root: '../../',
-  //    }),
-  //  ],
-  // },
 
   test: {
     reporters: ['default'],
     globals: true,
     cache: {
-      dir: '../../node_modules/.vitest',
+      dir: '../../../node_modules/.vitest',
     },
-    environment: 'jsdom',
+    environment: 'node',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
 });
