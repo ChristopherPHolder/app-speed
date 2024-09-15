14a717d8885b48cf0bddb8fd28d688889504b145
ChristopherPHolder <toholderandrews@gmail.com>
2024-09-15 08:25:43 +0200

    fix(runner): move runner to dir

--- packages/runner/app/vite.config.ts
+++ packages/runner/app/vite.config.ts
@@ -4,11 +4,11 @@ import { defineConfig } from 'vite';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  cacheDir: '../../node_modules/.vite/audit-runner',
+  cacheDir: '../../../node_modules/.vite/audit-runner',
 
   plugins: [
     viteTsConfigPaths({
-      root: '../../',
+      root: '../../../',
     }),
   ],
 
