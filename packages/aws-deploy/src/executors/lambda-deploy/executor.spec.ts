c624cf8a73cc11f52d84566c09afe476710e03ff
ChristopherPHolder <toholderandrews@gmail.com>
2024-12-13 12:17:59 +0100

    fix: migrate
    Signed-off-by: ChristopherPHolder <toholderandrews@gmail.com>

--- packages/aws-deploy/src/executors/lambda-deploy/executor.spec.ts
+++ packages/aws-deploy/src/executors/lambda-deploy/executor.spec.ts
@@ -1,12 +1,16 @@
-import { ExecutorContext } from '@nx/devkit';
+import { ExecutorContext, NxJsonConfiguration, ProjectGraph, ProjectsConfigurations } from '@nx/devkit';
 
 import { LambdaDeployExecutorSchema } from './schema';
 import executor from './executor';
+import { cwd } from 'node:process';
 
 const options: LambdaDeployExecutorSchema = {} as LambdaDeployExecutorSchema;
 const context: ExecutorContext = {
+  nxJsonConfiguration: undefined as unknown as NxJsonConfiguration<'*' | string[]>,
+  projectGraph: undefined as unknown as ProjectGraph,
+  projectsConfigurations: undefined as unknown as ProjectsConfigurations,
   root: '',
-  cwd: process.cwd(),
+  cwd: cwd(),
   isVerbose: false,
 };
 
