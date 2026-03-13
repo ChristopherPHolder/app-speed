9da47d15b09a5a0c78179c298367a118d5097e95
ChristopherPHolder <toholderandrews@gmail.com>
2026-03-13 19:24:58 +0100

    fix: delete dead code

--- libs/portal/feature-audit/src/index.ts
+++ libs/portal/feature-audit/src/index.ts
@@ -1,11 +1,8 @@
-export { UserFlowComponent as default, UserFlowComponent } from './lib/user-flow.component';
+export { UserFlowComponent } from './lib/user-flow.component';
 export { auditRoutes } from './lib/audit.routes';
 
 // Builder exports
 export { AuditBuilderContainer } from './lib/audit-builder/audit-builder.container';
-export { routes as builderRoutes } from './lib/audit-builder/route';
 
 // Viewer exports
 export { AuditViewerContainer } from './lib/container/audit-viewer.container';
-export { FlowResultComponent } from './lib/flow-result.component';
-export { ViewerComponent } from './lib/viewer.component';
