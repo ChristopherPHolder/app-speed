cdcbad6e3d85712d2348e36b9c6f07bfc4210262
ChristopherPHolder <toholderandrews@gmail.com>
2026-04-13 20:29:05 +0200

    fix: no puppeteer code import in portal

--- libs/audit/contracts/src/lib/audit-step.schema.ts
+++ libs/audit/contracts/src/lib/audit-step.schema.ts
@@ -1,22 +1,26 @@
 import { Schema } from 'effect';
-import { LIGHTHOUSE_AUDIT_STEP_TYPE } from '@app-speed/audit/model';
-import { StepType } from '@puppeteer/replay';
+import {
+  LIGHTHOUSE_AUDIT_STEP_TYPE,
+  PUPPETEER_REPLAY_ASSERTION_STEP_TYPE,
+  PUPPETEER_REPLAY_CUSTOM_STEP_TYPE,
+  PUPPETEER_REPLAY_USER_STEP_TYPE,
+} from '@app-speed/audit/model';
 
 const PuppeteerReplayStepTypeSchema = Schema.Literal(
-  StepType.Change,
-  StepType.Click,
-  StepType.Close,
-  StepType.CustomStep,
-  StepType.DoubleClick,
-  StepType.EmulateNetworkConditions,
-  StepType.Hover,
-  StepType.KeyDown,
-  StepType.KeyUp,
-  StepType.Navigate,
-  StepType.Scroll,
-  StepType.SetViewport,
-  StepType.WaitForElement,
-  StepType.WaitForExpression,
+  PUPPETEER_REPLAY_USER_STEP_TYPE.CHANGE,
+  PUPPETEER_REPLAY_USER_STEP_TYPE.CLICK,
+  PUPPETEER_REPLAY_USER_STEP_TYPE.CLOSE,
+  PUPPETEER_REPLAY_CUSTOM_STEP_TYPE.CUSTOM_STEP,
+  PUPPETEER_REPLAY_USER_STEP_TYPE.DOUBLE_CLICK,
+  PUPPETEER_REPLAY_USER_STEP_TYPE.EMULATE_NETWORK_CONDITIONS,
+  PUPPETEER_REPLAY_USER_STEP_TYPE.HOVER,
+  PUPPETEER_REPLAY_USER_STEP_TYPE.KEY_DOWN,
+  PUPPETEER_REPLAY_USER_STEP_TYPE.KEY_UP,
+  PUPPETEER_REPLAY_USER_STEP_TYPE.NAVIGATE,
+  PUPPETEER_REPLAY_USER_STEP_TYPE.SCROLL,
+  PUPPETEER_REPLAY_USER_STEP_TYPE.SET_VIEWPORT,
+  PUPPETEER_REPLAY_ASSERTION_STEP_TYPE.WAIT_FOR_ELEMENT,
+  PUPPETEER_REPLAY_ASSERTION_STEP_TYPE.WAIT_FOR_EXPRESSION,
 );
 
 const LighthouseUserflowStepTypeSchema = Schema.Literal(
