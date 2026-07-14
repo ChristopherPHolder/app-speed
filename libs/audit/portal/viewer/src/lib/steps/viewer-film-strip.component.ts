557b7e39e69abe2ecf87c6f7f7e19cc4c946d055
ChristopherPHolder <toholderandrews@gmail.com>
2026-07-14 06:26:54 +0200

    fix(viewer): improve desktop filmstrip

--- libs/audit/portal/viewer/src/lib/steps/viewer-film-strip.component.ts
+++ libs/audit/portal/viewer/src/lib/steps/viewer-film-strip.component.ts
@@ -7,33 +7,36 @@ import { ScrollContainerComponent } from '@app-speed/ui/scroll-container';
     <ui-scroll-container>
       <div class="film-strip-container">
         @for (item of filmStrip(); track item) {
-          <img class="film-strip-frame" [src]="item.data" height="100px" alt="" />
+          <img class="film-strip-frame" [src]="item.data" alt="" />
         }
       </div>
     </ui-scroll-container>
   `,
   styles: `
     :host {
+      container-type: inline-size;
       display: block;
-      padding: 20px 0 20px 0;
+      padding: 20px 0;
     }
+
     .film-strip-container {
       display: flex;
+      gap: 16px;
+      justify-content: space-between;
       overflow: auto;
       padding: 8px 4px;
     }
 
     .film-strip-frame {
+      flex: 0 0 auto;
+      height: clamp(100px, 15cqi, 160px);
+      width: auto;
       border: groove gray;
       box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
       border-radius: 8px;
       transition:
         transform 0.2s ease,
         box-shadow 0.2s ease;
-
-      &:not(:first-child):not(:last-child) {
-        margin: 0 8px;
-      }
 
       &:hover {
         transform: scale(1.05);
